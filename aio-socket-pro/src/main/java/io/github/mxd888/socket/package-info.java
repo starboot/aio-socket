64a7c61d3dbbeee6ee7c93491524f4f26502484e
MDong <1191998028@qq.com>
2022-07-28 17:53:11 +0800

    继续优化，保持完美

---
+++ aio-socket-pro/src/main/java/io/github/mxd888/socket/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Created by DELL(mxd) on 2022/7/28 17:38
+ */
+package io.github.mxd888.socket;
---
+++ aio-socket-pro/src/main/java/io/github/mxd888/socket/plugins/ACKPlugin.java
@@ -0,0 +1,7 @@
+package io.github.mxd888.socket.plugins;
+
+/**
+ * Created by DELL(mxd) on 2022/7/28 17:48
+ */
+public class ACKPlugin extends AbstractPlugin{
+}
