5cc7a314ce921e4b5460e47a3cc27a6097cd1660
MDong <1191998028@qq.com>
2022-08-05 13:47:39 +0800

    update core

--- aio-socket-core/src/main/java/io/github/mxd888/socket/utils/ThreadUtils.java
+++ aio-socket-core/src/main/java/io/github/mxd888/socket/utils/ThreadUtils.java
@@ -5,7 +5,7 @@ package io.github.mxd888.socket.utils;
 import java.util.concurrent.*;
 
 /**
- * 线程工具包
+ * 构造线程池执行器
  *
  * @author MDong
  * @version 2.10.1.v20211002-RELEASE
@@ -33,28 +33,39 @@ public class ThreadUtils {
     public static final long KEEP_ALIVE_TIME = 60L;
 
     /**
-     * 群组线程池执行器
-     */
-    private static ThreadPoolExecutor groupExecutor = null;
-
-    /**
      * 构造线程池执行器
      *
      * @return ExecutorService 线程池执行器
      */
     public static ThreadPoolExecutor getGroupExecutor() {
-        if (groupExecutor != null) {
-            return groupExecutor;
-        }
-        synchronized (ThreadUtils.class) {
-            if (groupExecutor != null) {
-                return groupExecutor;
-            }
-            LinkedBlockingQueue<Runnable> runnableQueue = new LinkedBlockingQueue<>();
-            ThreadFactory threadFactory = Executors.defaultThreadFactory();
-            groupExecutor = new ThreadPoolExecutor(MAX_POOL_SIZE_FOR_GROUP, MAX_POOL_SIZE_FOR_GROUP, KEEP_ALIVE_TIME, TimeUnit.SECONDS, runnableQueue, threadFactory);
-            groupExecutor.prestartCoreThread();
-            return groupExecutor;
-        }
+        return getGroupExecutor(MAX_POOL_SIZE_FOR_GROUP);
+    }
+
+    public static ThreadPoolExecutor getGroupExecutor(int threadNum) {
+        return getGroupExecutor(threadNum, KEEP_ALIVE_TIME, TimeUnit.SECONDS);
+    }
+
+    public static ThreadPoolExecutor getGroupExecutor(int threadNum,
+                                                      long keepAliveTime,
+                                                      TimeUnit timeUnit) {
+        LinkedBlockingQueue<Runnable> linkedBlockingQueue = new LinkedBlockingQueue<>();
+        return getGroupExecutor(threadNum, keepAliveTime, timeUnit, linkedBlockingQueue);
+    }
+
+    public static ThreadPoolExecutor getGroupExecutor(int threadNum,
+                                                      long keepAliveTime,
+                                                      TimeUnit timeUnit,
+                                                      BlockingQueue<Runnable> workQueue) {
+        return getGroupExecutor(threadNum, keepAliveTime, timeUnit, workQueue, Executors.defaultThreadFactory());
+    }
+
+    public static ThreadPoolExecutor getGroupExecutor(int threadNum,
+                                                      long keepAliveTime,
+                                                      TimeUnit timeUnit,
+                                                      BlockingQueue<Runnable> workQueue,
+                                                      ThreadFactory threadFactory) {
+        ThreadPoolExecutor groupExecutor = new ThreadPoolExecutor(threadNum, threadNum, keepAliveTime, timeUnit, workQueue, threadFactory);
+        groupExecutor.prestartCoreThread();
+        return groupExecutor;
     }
 }
