4484c511a61bc5f010241a31427de2c3babd8de9
MDong <1191998028@qq.com>
2022-10-04 20:14:05 +0800

    不重要的更新

--- aio-socket-kernel/src/main/java/io/github/mxd888/socket/intf/AioHandler.java
+++ aio-socket-kernel/src/main/java/io/github/mxd888/socket/intf/AioHandler.java
@@ -59,10 +59,10 @@ public interface AioHandler {
     /**
      * 状态机事件,当枚举事件发生时由框架触发该方法
      *
-     * @param channelContext   本次触发状态机的AioSession对象
-     * @param stateMachineEnum 状态枚举
-     * @param throwable        异常对象，如果存在的话
-     * @see StateMachineEnum
+     * @param channelContext                            本次触发状态机的AioSession对象
+     * @param stateMachineEnum                          状态枚举
+     * @param throwable                                 异常对象，如果存在的话
+     * @see io.github.mxd888.socket.StateMachineEnum    状态机枚举
      */
     default void stateEvent(ChannelContext channelContext, StateMachineEnum stateMachineEnum, Throwable throwable) {
         if (stateMachineEnum == StateMachineEnum.DECODE_EXCEPTION || stateMachineEnum == StateMachineEnum.PROCESS_EXCEPTION) {
