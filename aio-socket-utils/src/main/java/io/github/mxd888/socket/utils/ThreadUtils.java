4609318e8523fe3acf9dcc91dee1fa3326ecc259
MDong <1191998028@qq.com>
2022-10-08 18:59:11 +0800

    规范化

--- aio-socket-utils/src/main/java/io/github/mxd888/socket/utils/ThreadUtils.java
+++ aio-socket-utils/src/main/java/io/github/mxd888/socket/utils/ThreadUtils.java
@@ -30,16 +30,6 @@ import java.util.concurrent.*;
 public class ThreadUtils {
 
     /**
-     * aio-socket ThreadPool
-     */
-    private static ExecutorService aioExecutor;
-
-    /**
-     * Java8 ThreadPool
-     */
-    private static ExecutorService executorService;
-
-    /**
      * 保持活跃时间
      */
     public static final long KEEP_ALIVE_TIME = 0L;
@@ -62,32 +52,15 @@ public class ThreadUtils {
     /**
      * boss 最大内存池线程数
      */
-    public static final int MAX_POOL_SIZE_FOR_JAVA = Math.max(CORE_POOL_SIZE * 5, 10);
+    public static final int MAX_POOL_SIZE_FOR_BOSS = Math.max(CORE_POOL_SIZE * 5, 10);
 
     /**
      * worker 最大内存池线程数
      */
-    public static final int MAX_POOL_SIZE_FOR_AIO = MAX_POOL_SIZE_FOR_JAVA * 3;
+    public static final int MAX_POOL_SIZE_FOR_AIO_WORKER = MAX_POOL_SIZE_FOR_BOSS * 3;
 
-    /**
-     * 构造线程池执行器
-     * 用户调用getGroupExecutor()所返回的线程池是aio-socket框架在启动的时候已经创建完毕的;
-     * 若用户想创建新的、独立的ExecutorService请调用其他带有参数的方法
-     * 推荐使用getGroupExecutor()，和框架共用线程池会节省服务器资源
-     *
-     * @return ExecutorService 线程池执行器
-     */
     public static ExecutorService getGroupExecutor() {
-        if (executorService != null) {
-            return executorService;
-        }
-        synchronized (ThreadUtils.class) {
-            if (executorService != null) {
-                return executorService;
-            }
-            executorService = getGroupExecutor(MAX_POOL_SIZE_FOR_JAVA);
-        }
-        return executorService;
+        return getGroupExecutor(MAX_POOL_SIZE_FOR_BOSS);
     }
 
     public static ExecutorService getGroupExecutor(int corePoolSize) {
@@ -139,25 +112,8 @@ public class ThreadUtils {
         return groupExecutor;
     }
 
-    /**
-     * aio-socket-kernel 框架使用的先来先服务线程池;
-     * 用户调用getAioExecutor()所返回的线程池是aio-socket框架在启动的时候已经创建完毕的;
-     * 若用户想创建新的、独立的AioExecutor请调用其他带有参数的方法
-     * 推荐使用getAioExecutor()，和框架共用线程池会节省服务器资源
-     *
-     * @return 内核线程池
-     */
     public static ExecutorService getAioExecutor() {
-        if (aioExecutor != null) {
-            return aioExecutor;
-        }
-        synchronized (ThreadUtils.class) {
-            if (aioExecutor != null) {
-                return aioExecutor;
-            }
-            aioExecutor = getAioExecutor(MAX_POOL_SIZE_FOR_AIO);
-            return aioExecutor;
-        }
+        return getAioExecutor(MAX_POOL_SIZE_FOR_AIO_WORKER);
     }
 
     public static ExecutorService getAioExecutor(int corePoolSize) {
@@ -182,8 +138,8 @@ public class ThreadUtils {
                                                  long keepAliveTime,
                                                  TimeUnit timeUnit,
                                                  BlockingQueue<Runnable> runnableQueue) {
-        return getAioExecutor(corePoolSize, maxPoolSize, keepAliveTime, timeUnit,
-                runnableQueue, DefaultThreadFactory.getInstance(defaultThreadName, Thread.MAX_PRIORITY));
+        return getAioExecutor(corePoolSize, maxPoolSize, keepAliveTime, timeUnit, runnableQueue,
+                DefaultThreadFactory.getInstance(defaultThreadName, Thread.MAX_PRIORITY));
     }
 
     public static ExecutorService getAioExecutor(int corePoolSize,
