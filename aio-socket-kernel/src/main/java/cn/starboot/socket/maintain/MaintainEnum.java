9284db5b21bede5cb0ec0652615233356b783a1c
MDong <1191998028@qq.com>
2023-04-25 12:49:35 +0800

    updating

---
+++ aio-socket-kernel/src/main/java/cn/starboot/socket/maintain/MaintainEnum.java
@@ -0,0 +1,23 @@
+package cn.starboot.socket.maintain;
+
+/**
+ * aio-socket通道关系维护枚举类
+ *
+ * @author MDong
+ */
+public enum MaintainEnum {
+
+	BUSINESS_ID,
+
+	CLIENT_NODE_ID,
+
+	GROUP_ID,
+
+	USER_ID,
+
+	IP,
+
+	TOKEN,
+
+	USER;
+}
