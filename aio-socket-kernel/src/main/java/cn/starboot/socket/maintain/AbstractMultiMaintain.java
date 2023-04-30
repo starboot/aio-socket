91390ac1c032e428c77274c77ee6966288124a8c
MDong <1191998028@qq.com>
2023-04-30 15:23:50 +0800

    updating

--- aio-socket-kernel/src/main/java/cn/starboot/socket/maintain/AbstractMultiMaintain.java
+++ aio-socket-kernel/src/main/java/cn/starboot/socket/maintain/AbstractMultiMaintain.java
@@ -44,11 +44,15 @@ public abstract class AbstractMultiMaintain extends AbstractMaintain {
 	 */
 	@Override
 	public boolean remove(String id, ChannelContext context) {
-		if (Objects.isNull(id) || id.equals("") || id.length() == 0 || Objects.isNull(context)) {
+		if (Objects.isNull(id)
+				|| id.equals("")
+				|| id.length() == 0
+				|| Objects.isNull(context)) {
 			return false;
 		}
 		boolean result;
-		if (Objects.isNull(getMultiMaintainMap().get(id)) && !(getMultiMaintainMap().get(id).getObj().contains(context))) {
+		if (Objects.isNull(getMultiMaintainMap().get(id))
+				&& !(getMultiMaintainMap().get(id).getObj().contains(context))) {
 			return true;
 		}else {
 			result = getMultiMaintainMap().get(id).remove(context);
