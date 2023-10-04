bedcf281110d96952ca1aaee92aa302d434b6841
MDong <1191998028@qq.com>
2023-10-04 14:35:52 +0800

    welcome to v3.0

--- aio-socket-demo/src/main/java/cn/starboot/socket/demo/batch/stream/Client.java
+++ aio-socket-demo/src/main/java/cn/starboot/socket/demo/batch/stream/Client.java
@@ -17,47 +17,14 @@ package cn.starboot.socket.demo.batch.stream;
 
 import cn.starboot.socket.Packet;
 import cn.starboot.socket.codec.bytes.BytesPacket;
-import cn.starboot.socket.codec.string.StringPacket;
 import cn.starboot.socket.core.Aio;
 import cn.starboot.socket.core.ChannelContext;
 import cn.starboot.socket.core.ClientBootstrap;
 import cn.starboot.socket.jdk.aio.ImproveAsynchronousChannelGroup;
 import cn.starboot.socket.utils.ThreadUtils;
-import cn.starboot.socket.utils.pool.memory.MemoryPool;
-import cn.starboot.socket.utils.pool.memory.MemoryPoolFactory;
 
 import java.io.IOException;
-import java.nio.channels.AsynchronousChannelGroup;
-import java.util.concurrent.ExecutorService;
 
-/**
- * ------------------------------------------------
- * 			aio-socket performance
- * -------------------in 5 sec --------------------
- * inflow:				1422.926399230957(MB)
- * outflow:			    1421.8663444519043(MB)
- * process fail:		0
- * process count:		16207917
- * process total:		141181524
- * read count:			1423
- * write count:		    364174
- * connect count:		0
- * disconnect count:	0
- * online count:		10
- * connected total:	    10
- * Requests/sec:		3241583.4
- * Transfer/sec:		284.5852798461914(MB)
- * ------------------------------------------------
- *
- * Exception in thread "Thread-4" java.lang.NullPointerException
- * 	at cn.starboot.socket.core.WriteBuffer.flush(WriteBuffer.java:276)
- * 	at cn.starboot.socket.core.ChannelContext.flush(ChannelContext.java:135)
- * 	at cn.starboot.socket.core.TCPChannelContext.aioEncoder(TCPChannelContext.java:362)
- * 	at cn.starboot.socket.core.Aio.send0(Aio.java:1543)
- * 	at cn.starboot.socket.core.Aio.send(Aio.java:1526)
- * 	at cn.starboot.socket.demo.batch.stream.Client.lambda$main$0(Client.java:71)
- * 	at java.lang.Thread.run(Thread.java:748)
- */
 public class Client {
 
     public static void main(String[] args) throws IOException {
