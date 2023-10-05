c5dbb2a4b495e5dd3ab8bc0d685a2a66f8dfc1d3
MDong <1191998028@qq.com>
2023-10-05 21:08:05 +0800

    welcome to v3.0 (处理空轮训)

--- aio-socket-kernel/src/main/java/cn/starboot/socket/jdk/nio/ImproveNioSelector.java
+++ aio-socket-kernel/src/main/java/cn/starboot/socket/jdk/nio/ImproveNioSelector.java
@@ -1,8 +1,192 @@
 package cn.starboot.socket.jdk.nio;
 
-import java.nio.channels.Selector;
+import cn.starboot.socket.utils.SystemPropertyUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-public class ImproveNioSelector {
+import java.io.IOException;
+import java.nio.channels.SelectionKey;
+import java.nio.channels.Selector;
+import java.nio.channels.spi.SelectorProvider;
+import java.util.Set;
+
+/**
+ * 解决一下NIO空轮训的bug(虽然官方已经表明修复成功，但部分网友仍坚持还有出现的可能)
+ *
+ * @author MDong
+ */
+final class ImproveNioSelector extends Selector {
+
+	private static final Logger LOGGER = LoggerFactory.getLogger(ImproveNioSelector.class);
 
 	private Selector selector;
+
+	private static final int MIN_PREMATURE_SELECTOR_RETURNS = 3;
+
+	private static final int SELECTOR_AUTO_REBUILD_THRESHOLD;
+
+	static {
+		int selectorAutoRebuildThreshold = SystemPropertyUtil.getInt("cn.starboot.socket.selectorAutoRebuildThreshold", 512);
+		if (selectorAutoRebuildThreshold < MIN_PREMATURE_SELECTOR_RETURNS) {
+			selectorAutoRebuildThreshold = 0;
+		}
+
+		SELECTOR_AUTO_REBUILD_THRESHOLD = selectorAutoRebuildThreshold;
+
+		if (LOGGER.isDebugEnabled()) {
+			LOGGER.debug("-Dio.nio.selectorAutoRebuildThreshold: {}", SELECTOR_AUTO_REBUILD_THRESHOLD);
+		}
+	}
+
+	private ImproveNioSelector() {
+		try {
+			this.selector = openSelector();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static ImproveNioSelector open() throws IOException {
+		return new ImproveNioSelector();
+	}
+
+	@Override
+	public boolean isOpen() {
+		return selector.isOpen();
+	}
+
+	@Override
+	public SelectorProvider provider() {
+		return selector.provider();
+	}
+
+	@Override
+	public Set<SelectionKey> keys() {
+		return selector.keys();
+	}
+
+	@Override
+	public Set<SelectionKey> selectedKeys() {
+		return selector.selectedKeys();
+	}
+
+	@Override
+	public int selectNow() throws IOException {
+		return selector.selectNow();
+	}
+
+	@Override
+	public int select(long timeout) throws IOException {
+		return selector.select(timeout);
+	}
+
+	int selectCnt = 0;
+	@Override
+	public int select() throws IOException {
+		selectCnt++;
+		boolean ranTasks = true;
+		if (ranTasks) {  // ranTasks || strategy > 0
+			if (selectCnt > MIN_PREMATURE_SELECTOR_RETURNS && LOGGER.isDebugEnabled()) {
+				LOGGER.debug("Selector.select() returned prematurely {} times in a row for Selector {}.",
+						selectCnt - 1, selector);
+			}
+			selectCnt = 0;
+		} else if (unexpectedSelectorWakeup(selectCnt)) { // Unexpected wakeup (unusual case)
+			selectCnt = 0;
+		}
+		return selector.select();
+	}
+
+	@Override
+	public Selector wakeup() {
+		return selector.wakeup();
+	}
+
+	@Override
+	public void close() throws IOException {
+		selector.close();
+	}
+
+
+	// returns true if selectCnt should be reset
+	private boolean unexpectedSelectorWakeup(int selectCnt) {
+		if (Thread.interrupted()) {
+			// Thread was interrupted so reset selected keys and break so we not run into a busy loop.
+			// As this is most likely a bug in the handler of the user or it's client library we will
+			// also log it.
+			//
+			// See /issues/2426
+			if (LOGGER.isDebugEnabled()) {
+				LOGGER.debug("Selector.select() returned prematurely because " +
+						"Thread.currentThread().interrupt() was called. Use " +
+						"NioEventLoop.shutdownGracefully() to shutdown the NioEventLoop.");
+			}
+			return true;
+		}
+		if (SELECTOR_AUTO_REBUILD_THRESHOLD > 0 &&
+				selectCnt >= SELECTOR_AUTO_REBUILD_THRESHOLD) {
+			// The selector returned prematurely many times in a row.
+			// Rebuild the selector to work around the problem.
+			LOGGER.warn("Selector.select() returned prematurely {} times in a row; rebuilding Selector {}.",
+					selectCnt, selector);
+			rebuildSelector();
+			return true;
+		}
+		return false;
+	}
+
+	private void rebuildSelector() {
+		final Selector oldSelector = selector;
+		final Selector newSelector;
+
+		if (oldSelector == null) {
+			return;
+		}
+
+		try {
+			newSelector = openSelector();
+		} catch (Exception e) {
+			LOGGER.warn("Failed to create a new Selector.", e);
+			return;
+		}
+
+		// Register all channels to the new Selector.
+		int nChannels = 0;
+		for (SelectionKey key: oldSelector.keys()) {
+			Object a = key.attachment();
+			try {
+				if (!key.isValid() || key.channel().keyFor(newSelector) != null) {
+					continue;
+				}
+
+				int interestOps = key.interestOps();
+				key.cancel();
+				key.channel().register(newSelector, interestOps, a);
+				nChannels ++;
+			} catch (Exception e) {
+				LOGGER.warn("Failed to re-register a Channel to the new Selector.", e);
+			}
+		}
+
+		selector = newSelector;
+
+		try {
+			// time to close the old selector as everything else is registered to the new one
+			oldSelector.close();
+		} catch (Throwable t) {
+			if (LOGGER.isWarnEnabled()) {
+				LOGGER.warn("Failed to close the old Selector.", t);
+			}
+		}
+
+		if (LOGGER.isInfoEnabled()) {
+			LOGGER.info("Migrated " + nChannels + " channel(s) to the new Selector.");
+		}
+	}
+
+	private Selector openSelector() throws IOException {
+		return Selector.open();
+	}
+
+
 }
---
+++ aio-socket-kernel/src/main/java/cn/starboot/socket/utils/SystemPropertyUtil.java
@@ -0,0 +1,168 @@
+package cn.starboot.socket.utils;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.security.AccessController;
+import java.security.PrivilegedAction;
+import java.util.Objects;
+
+public class SystemPropertyUtil {
+
+	private static final Logger LOGGER = LoggerFactory.getLogger(SystemPropertyUtil.class);
+
+	/**
+	 * Returns {@code true} if and only if the system property with the specified {@code key}
+	 * exists.
+	 */
+	public static boolean contains(String key) {
+		return get(key) != null;
+	}
+
+	/**
+	 * Returns the value of the Java system property with the specified
+	 * {@code key}, while falling back to {@code null} if the property access fails.
+	 *
+	 * @return the property value or {@code null}
+	 */
+	public static String get(String key) {
+		return get(key, null);
+	}
+
+	/**
+	 * Returns the value of the Java system property with the specified
+	 * {@code key}, while falling back to the specified default value if
+	 * the property access fails.
+	 *
+	 * @return the property value.
+	 *         {@code def} if there's no such property or if an access to the
+	 *         specified property is not allowed.
+	 */
+	public static String get(final String key, String def) {
+//		checkNonEmpty(key, "key");
+		if (Objects.isNull(key))
+			return null;
+
+		String value = null;
+		try {
+			if (System.getSecurityManager() == null) {
+				value = System.getProperty(key);
+			} else {
+				value = AccessController.doPrivileged(new PrivilegedAction<String>() {
+					@Override
+					public String run() {
+						return System.getProperty(key);
+					}
+				});
+			}
+		} catch (SecurityException e) {
+			LOGGER.warn("Unable to retrieve a system property '{}'; default values will be used.", key, e);
+		}
+
+		if (value == null) {
+			return def;
+		}
+
+		return value;
+	}
+
+	/**
+	 * Returns the value of the Java system property with the specified
+	 * {@code key}, while falling back to the specified default value if
+	 * the property access fails.
+	 *
+	 * @return the property value.
+	 *         {@code def} if there's no such property or if an access to the
+	 *         specified property is not allowed.
+	 */
+	public static boolean getBoolean(String key, boolean def) {
+		String value = get(key);
+		if (value == null) {
+			return def;
+		}
+
+		value = value.trim().toLowerCase();
+		if (value.isEmpty()) {
+			return def;
+		}
+
+		if ("true".equals(value) || "yes".equals(value) || "1".equals(value)) {
+			return true;
+		}
+
+		if ("false".equals(value) || "no".equals(value) || "0".equals(value)) {
+			return false;
+		}
+
+		LOGGER.warn(
+				"Unable to parse the boolean system property '{}':{} - using the default value: {}",
+				key, value, def
+		);
+
+		return def;
+	}
+
+	/**
+	 * Returns the value of the Java system property with the specified
+	 * {@code key}, while falling back to the specified default value if
+	 * the property access fails.
+	 *
+	 * @return the property value.
+	 *         {@code def} if there's no such property or if an access to the
+	 *         specified property is not allowed.
+	 */
+	public static int getInt(String key, int def) {
+		String value = get(key);
+		if (value == null) {
+			return def;
+		}
+
+		value = value.trim();
+		try {
+			return Integer.parseInt(value);
+		} catch (Exception e) {
+			// Ignore
+		}
+
+		LOGGER.warn(
+				"Unable to parse the integer system property '{}':{} - using the default value: {}",
+				key, value, def
+		);
+
+		return def;
+	}
+
+	/**
+	 * Returns the value of the Java system property with the specified
+	 * {@code key}, while falling back to the specified default value if
+	 * the property access fails.
+	 *
+	 * @return the property value.
+	 *         {@code def} if there's no such property or if an access to the
+	 *         specified property is not allowed.
+	 */
+	public static long getLong(String key, long def) {
+		String value = get(key);
+		if (value == null) {
+			return def;
+		}
+
+		value = value.trim();
+		try {
+			return Long.parseLong(value);
+		} catch (Exception e) {
+			// Ignore
+		}
+
+		LOGGER.warn(
+				"Unable to parse the long integer system property '{}':{} - using the default value: {}",
+				key, value, def
+		);
+
+		return def;
+	}
+
+	private SystemPropertyUtil() {
+		// Unused
+	}
+}
