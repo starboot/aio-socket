087c4fad11e04494e34a41f1cbafe94889f390bb
MDong <1191998028@qq.com>
2023-10-13 21:17:40 +0800

    update

--- aio-socket-utils/src/main/java/cn/starboot/socket/utils/pool/memory/MemoryBlock.java
+++ aio-socket-utils/src/main/java/cn/starboot/socket/utils/pool/memory/MemoryBlock.java
@@ -1,18 +1,11 @@
-/*
- *    Copyright 2019 The aio-socket Project
+/*******************************************************************************
+ * Copyright (c) 2017-2019, org.smartboot. All rights reserved.
+ * project name: smart-socket
+ * file name: Protocol.java
+ * Date: 2019-12-31
+ * Author: sandao (zhengjunweimail@163.com)
  *
- *    The aio-socket Project Licenses this file to you under the Apache License,
- *    Version 2.0 (the "License"); you may not use this file except in compliance
- *    with the License. You may obtain a copy of the License at:
- *
- *        http://www.apache.org/licenses/LICENSE-2.0
- *
- *    Unless required by applicable law or agreed to in writing, software
- *    distributed under the License is distributed on an "AS IS" BASIS,
- *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *    See the License for the specific language governing permissions and
- *    limitations under the License.
- */
+ ******************************************************************************/
 package cn.starboot.socket.utils.pool.memory;
 
 import sun.nio.ch.DirectBuffer;
@@ -25,6 +18,7 @@ import java.util.concurrent.locks.ReentrantLock;
 /**
  * ByteBuffer内存页
  *
+ * @author smart-socket: https://gitee.com/smartboot/smart-socket.git
  * @author MDong
  * @version 2.10.1.v20211002-RELEASE
  */
