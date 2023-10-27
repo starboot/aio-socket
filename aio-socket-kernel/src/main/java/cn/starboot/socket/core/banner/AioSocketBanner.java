f699a609873cc7f7d2e34a42a72feee0aa061954
MDong <1191998028@qq.com>
2023-10-27 15:59:03 +0800

    update

--- aio-socket-kernel/src/main/java/cn/starboot/socket/core/banner/AioSocketBanner.java
+++ aio-socket-kernel/src/main/java/cn/starboot/socket/core/banner/AioSocketBanner.java
@@ -7,13 +7,10 @@ import java.io.PrintStream;
 public class AioSocketBanner implements Banner {
 
 	private static final String BANNER =
-					"  _______       _____  ____    ____  \n" +
-					" |__   __|     |_   _||_   \\  /   _| \n" +
-					"    | |  ______  | |    |   \\/   |   \n" +
-					"    | | |______| | |    | |\\  /| |   \n" +
-					"    | |         _| |_  _| |_\\/_| |_  \n" +
-					"    |_|        |_____||_____||_____| \n" +
-					" ";
+					"   _     ___    ___          ___    ___     ___   _  __  ___   _____ \n" +
+					"  /_\\   |_ _|  / _ \\   ___  / __|  / _ \\   / __| | |/ / | __| |_   _|\n" +
+					" / _ \\   | |  | (_) | |___| \\__ \\ | (_) | | (__  | ' <  | _|    | |  \n" +
+					"/_/ \\_\\ |___|  \\___/        |___/  \\___/   \\___| |_|\\_\\ |___|   |_|  \n";
 
 	@Override
 	public void printBanner(PrintStream printStream) {
@@ -22,6 +19,6 @@ public class AioSocketBanner implements Banner {
 	}
 
 	private String getTimVersion() {
-		return "\033[036m :: "+ AioConfig.PROJECT_NAME +" :: \033[0m" + " Kernel version: " + AioConfig.VERSION;
+		return "\033[036m :: " + AioConfig.PROJECT_NAME + " :: \033[0m" + " Kernel version: " + AioConfig.VERSION;
 	}
 }
