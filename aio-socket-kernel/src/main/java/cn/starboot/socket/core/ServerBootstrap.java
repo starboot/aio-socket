852eac6b36806e6635d64aa8a0da870273d582b9
MDong <1191998028@qq.com>
2023-10-27 15:27:06 +0800

    update

--- aio-socket-kernel/src/main/java/cn/starboot/socket/core/ServerBootstrap.java
+++ aio-socket-kernel/src/main/java/cn/starboot/socket/core/ServerBootstrap.java
@@ -1,8 +1,35 @@
 package cn.starboot.socket.core;
 
+import cn.starboot.socket.core.spi.KernelBootstrapProvider;
+
+/**
+ * aio-socket Server BootStrap
+ *
+ * @author MDong
+ */
 public abstract class ServerBootstrap {
 
+	private final KernelBootstrapProvider kernelBootstrapProvider;
+
+	protected ServerBootstrap(KernelBootstrapProvider kernelBootstrapProvider) {
+		this.kernelBootstrapProvider = kernelBootstrapProvider;
+	}
+
+	public final KernelBootstrapProvider provider() {
+		return kernelBootstrapProvider;
+	}
+
+	/**
+	 * 启动TCP服务
+	 */
 	public static void startTCPService() {
+
+	}
+
+	/**
+	 * 启动UDP服务
+	 */
+	public static void startUDPService() {
 
 	}
 }
