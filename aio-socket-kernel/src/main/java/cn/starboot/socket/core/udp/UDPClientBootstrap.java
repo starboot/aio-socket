3bc54b8cdab7e7fb89e1046e19744ce820525eac
MDong <1191998028@qq.com>
2023-10-27 13:13:22 +0800

    update

--- aio-socket-kernel/src/main/java/cn/starboot/socket/core/udp/UDPClientBootstrap.java
+++ aio-socket-kernel/src/main/java/cn/starboot/socket/core/udp/UDPClientBootstrap.java
@@ -5,6 +5,11 @@ import cn.starboot.socket.core.intf.AioHandler;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+/**
+ * UDP客户端引导程序
+ *
+ * @author MDong
+ */
 public final class UDPClientBootstrap extends UDPBootstrap {
 
 	private static final Logger LOGGER = LoggerFactory.getLogger(UDPClientBootstrap.class);
--- aio-socket-kernel/src/main/java/cn/starboot/socket/core/udp/UDPServerBootstrap.java
+++ aio-socket-kernel/src/main/java/cn/starboot/socket/core/udp/UDPServerBootstrap.java
@@ -5,6 +5,11 @@ import cn.starboot.socket.core.intf.AioHandler;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+/**
+ * UDP服务器引导程序
+ *
+ * @author MDong
+ */
 public final class UDPServerBootstrap extends UDPBootstrap {
 
 	private static final Logger LOGGER = LoggerFactory.getLogger(UDPServerBootstrap.class);
