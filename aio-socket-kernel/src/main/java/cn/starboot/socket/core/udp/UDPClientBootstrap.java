5512c723c1cff3a22d9d4a4c7ca53f6744d8b79e
MDong <1191998028@qq.com>
2023-10-28 21:40:29 +0800

    update

--- aio-socket-kernel/src/main/java/cn/starboot/socket/core/udp/UDPClientBootstrap.java
+++ aio-socket-kernel/src/main/java/cn/starboot/socket/core/udp/UDPClientBootstrap.java
@@ -4,6 +4,7 @@ import cn.starboot.socket.core.ChannelContext;
 import cn.starboot.socket.core.ClientBootstrap;
 import cn.starboot.socket.core.Packet;
 import cn.starboot.socket.core.config.AioClientConfig;
+import cn.starboot.socket.core.enums.ProtocolEnum;
 import cn.starboot.socket.core.intf.AioHandler;
 import cn.starboot.socket.core.jdk.aio.ImproveAsynchronousChannelGroup;
 import cn.starboot.socket.core.plugins.Plugin;
@@ -12,6 +13,9 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.net.SocketAddress;
+import java.nio.channels.DatagramChannel;
 
 /**
  * UDP客户端引导程序
@@ -22,72 +26,128 @@ final class UDPClientBootstrap extends UDPBootstrap implements ClientBootstrap {
 
 	private static final Logger LOGGER = LoggerFactory.getLogger(UDPClientBootstrap.class);
 
+	private DatagramChannel clientDatagramChannel;
+
+	/**
+	 * 绑定本地地址
+	 */
+	private SocketAddress localAddress;
+
+	/**
+	 * 客户端所用协议
+	 */
+	private ProtocolEnum clientProtocol;
+
+	/**
+	 * 心跳包
+	 */
+	private Packet heartBeat = null;
+
 	UDPClientBootstrap(UDPKernelBootstrapProvider udpKernelBootstrapProvider, KernelBootstrapProvider kernelBootstrapProvider) {
 		super(new AioClientConfig(), udpKernelBootstrapProvider, kernelBootstrapProvider);
 	}
 
 	@Override
 	public ChannelContext start() throws IOException {
-		return null;
+		return start(null);
 	}
 
 	@Override
 	public ChannelContext start(ImproveAsynchronousChannelGroup asynchronousChannelGroup) throws IOException {
+
 		return null;
 	}
 
 	@Override
-	public void shutdownNow() {
+	public final void shutdown() {
+		shutdown0(false);
+	}
 
+	/**
+	 * 立即关闭客户端
+	 */
+	@Override
+	public final void shutdownNow() {
+		shutdown0(true);
+	}
+
+	private void shutdown0(boolean flag) {
+//		if (this.channelContext != null) {
+//			this.channelContext.close(flag);
+//			this.channelContext = null;
+//		}
+		super.shutdown();
 	}
 
 	@Override
 	public ClientBootstrap listen(int port) {
-		return null;
+		this.localAddress = new InetSocketAddress(port);
+		return this;
 	}
 
 	@Override
 	public ClientBootstrap remote(String host, int port) {
-		return null;
+		getConfig().setHost(host);
+		getConfig().setPort(port);
+		return this;
 	}
 
 	@Override
 	public ClientBootstrap addHeartPacket(Packet heartPacket) {
-		return null;
+		this.heartBeat = heartPacket;
+		return this;
 	}
 
 	@Override
 	public ClientBootstrap setThreadNum(int bossThreadNum) {
-		return null;
+		getConfig().setBossThreadNumber(bossThreadNum);
+		return this;
 	}
 
 	@Override
 	public ClientBootstrap setMemoryPoolFactory(int size, int num, boolean useDirect) {
-		return null;
+		getConfig().setDirect(useDirect).setMemoryBlockSize(size).setMemoryBlockNum(num);
+		return this;
 	}
 
 	@Override
 	public ClientBootstrap setWriteBufferSize(int writeBufferSize, int maxWaitNum) {
-		return null;
+		getConfig().setWriteBufferSize(writeBufferSize)
+				.setMaxWaitNum(maxWaitNum);
+		return this;
 	}
 
 	@Override
 	public ClientBootstrap setReadBufferSize(int readBufferSize) {
-		return null;
+		getConfig().setReadBufferSize(readBufferSize);
+		return this;
 	}
 
 	@Override
 	public ClientBootstrap setMemoryKeep(boolean isMemoryKeep) {
-		return null;
+		getConfig().setMemoryKeep(isMemoryKeep);
+		return this;
 	}
 
 	@Override
 	public ClientBootstrap addPlugin(Plugin plugin) {
-		return null;
+		getConfig().getPlugins().addPlugin(plugin);
+		return this;
 	}
 
 	@Override
-	public ClientBootstrap addAioHandler(AioHandler handler) {
-		return null;
+	public synchronized ClientBootstrap addAioHandler(AioHandler handler) {
+		if (this.clientProtocol != null) {
+			String err = "ClientBootstrap can only call addAioHandler once";
+			if (LOGGER.isErrorEnabled()) {
+				LOGGER.error(err);
+			}else {
+				System.err.println(err);
+			}
+			return this;
+		}
+		getConfig().getPlugins().addAioHandler(handler);
+		this.clientProtocol = handler.name();
+		return this;
 	}
 }
