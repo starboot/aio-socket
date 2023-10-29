88411a7d81ab754fa974f51853dfd3b5fdfae330
MDong <1191998028@qq.com>
2023-10-29 17:24:58 +0800

    update

--- aio-socket-kernel/src/main/java/cn/starboot/socket/core/udp/UDPWriteWorker.java
+++ aio-socket-kernel/src/main/java/cn/starboot/socket/core/udp/UDPWriteWorker.java
@@ -1,22 +1,39 @@
 package cn.starboot.socket.core.udp;
 
+import cn.starboot.socket.core.utils.pool.memory.MemoryUnit;
+
 import java.util.concurrent.ConcurrentLinkedQueue;
+import java.util.concurrent.Semaphore;
 
 final class UDPWriteWorker implements Runnable {
 
-	private final ConcurrentLinkedQueue<?> w = new ConcurrentLinkedQueue<>();
+	private final ConcurrentLinkedQueue<MemoryUnit> writeQueue = new ConcurrentLinkedQueue<>();
 
 	private boolean isRunning = true;
 
+	private Semaphore semaphore = new Semaphore(1);
+
 	void shutdownWriteWorker() {
 		isRunning = false;
+	}
+
+	void addMemoryUnitToWriteQueue(MemoryUnit WriteMemoryUnit) {
+		writeQueue.offer(WriteMemoryUnit);
 	}
 
 	@Override
 	public void run() {
 
 		while (isRunning) {
-			// todo
+			if (writeQueue.poll() != null) {
+				//
+			} else {
+				try {
+					semaphore.acquire();
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+			}
 		}
 	}
 }
